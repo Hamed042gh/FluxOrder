63054835fc4cf644fb0de0bfc296698c684ee0d0
HamedGhasemi <blearn704@gmail.com>
2025-02-17 22:07:01 +0100

    [Feat]: Add store, update, and delete methods for orders Implemented storeOrder, updateOrder, and destroyOrder methods in OrderController. Integrated validation requests (OrderRequest and UpdateOrderRequest) for input validation. Updated routes to handle the new actions for creating, updating, and deleting orders. Protected  routes to ensure only authenticated users can access or modify order data.

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\OrderRequest;
+use App\Http\Requests\UpdateOrderRequest;
 use App\Models\Order;
 use Illuminate\Support\Facades\DB;
 
@@ -45,5 +47,64 @@ class OrderController extends Controller
             ],
             'message' => 'Order fetched successfully.'
         ], 200);
+    }
+
+    public function storeOrder(OrderRequest $request)
+    {
+       
+        try {
+            $order = Order::create($request->all());
+
+            return response()->json([
+                'order' => $order,
+                'message' => 'Order created successfully.'
+            ], 201);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'An error occurred while creating order. Please try again later.',
+                'details' => $e->getMessage()
+            ], 500);
+        }
+    }
+    
+    public function updateOrder(UpdateOrderRequest $request,$id)
+    {
+       
+        $order = Order::findOrfail($id);
+
+        try {
+            $order->update($request->all());
+
+            return response()->json([
+                'order' => $order,
+                'message' => 'Order updated successfully.'
+            ], 200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'An error occurred while updating order. Please try again later.',
+                'details' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function destroyOrder($id)
+    {
+        $order = Order::findOrfail($id);
+
+        try {
+            $order->delete();
+
+            return response()->json([
+               'message' => 'Order deleted successfully.'
+            ], 204);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'An error occurred while deleting order. Please try again later.',
+                'details' => $e->getMessage()
+            ], 500);
+        }
     }
 }

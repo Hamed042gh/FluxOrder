61709f52258be5fae777ba03423132ca8acb70ca
HamedGhasemi <blearn704@gmail.com>
2025-02-21 18:07:24 +0100

    [Feat]: Adjust admin and super admin permissions
    - **Admin** can create and edit users but cannot delete them.
    - **Admin** can only view roles and permissions.
    - **Super Admin** has full control over users, roles, and permissions.

--- app/Policies/RolePolicy.php
+++ app/Policies/RolePolicy.php
@@ -13,7 +13,7 @@ class RolePolicy
      */
     public function viewAny(User $user): bool
     {
-        return false;
+        return $user->hasRole(['Admin', 'SuperAdmin']);
     }
 
     /**
@@ -29,7 +29,7 @@ class RolePolicy
      */
     public function create(User $user): bool
     {
-        return false;
+        return $user->hasRole('SuperAdmin');
     }
 
     /**
@@ -37,7 +37,7 @@ class RolePolicy
      */
     public function update(User $user, Role $role): bool
     {
-        return false;
+        return $user->hasRole('SuperAdmin');
     }
 
     /**
@@ -45,7 +45,7 @@ class RolePolicy
      */
     public function delete(User $user, Role $role): bool
     {
-        return false;
+        return $user->hasRole('SuperAdmin');
     }
 
     /**
